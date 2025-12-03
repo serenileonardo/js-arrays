3edcb364541e945ca4b911e3df4b9598358dccff
serenileonardo <serenileonardo.web@gmail.com>
2025-12-03 14:11:10 +0100

    feat: finish

--- 2nd exercise/script.js
+++ 2nd exercise/script.js
@@ -10,17 +10,17 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.slice().reverse();
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(name => name.length >= 5);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-
+teachers = teachers.filter(name => name !== 'Ed');
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(', ');
